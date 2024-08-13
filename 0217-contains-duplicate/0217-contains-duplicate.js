272c1bcb6468f44fcf5844ea57abd730d9940923
fangfang <25191617+f2xiao@users.noreply.github.com>
2024-08-13 15:46:10 -0400

    Time: 156 ms (5.61%), Space: 59 MB (97.92%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,16 +2,30 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// var containsDuplicate = function(nums) {
+//     const map = {};
+//     nums.forEach((ele, index, arr) => {
+//         // check if map contains the key
+//         if(!map[ele]){
+//             map[ele] = 1;
+//         }else{
+//             map[ele] ++;
+//         }
+//     })
+//     // check the values in map all are 1
+//     return !Object.values(map).every(ele=>ele < 2)
+// };
+
 var containsDuplicate = function(nums) {
-    const map = {};
-    nums.forEach((ele, index, arr) => {
-        // check if map contains the key
-        if(!map[ele]){
-            map[ele] = 1;
-        }else{
-            map[ele] ++;
-        }
-    })
-    // check the values in map all are 1
-    return !Object.values(map).every(ele=>ele < 2)
+    // sort the nums
+   nums.sort();
+
+   for(let i=0; i < nums.length ; i++){
+    //  compare the neighbour element
+    if(nums[i] == nums[i+1]){
+        return true
+    }
+   }
+   
+   return false
 };
