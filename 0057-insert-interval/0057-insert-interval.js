8a1c55b79481be9da4e88ce7ddd1f98e16629d3c
fangfang <25191617+f2xiao@users.noreply.github.com>
2024-10-16 11:30:23 -0400

    Time: 82 ms (26.06%), Space: 54.1 MB (67.76%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -4,10 +4,14 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-    // add the newInterval then sort it
+    // add `newInterval` to `intervals` then sort the results based on the start of each interval
     let newIntervals = [...intervals, newInterval].sort((a,b)=>a[0] - b[0]);
     let mergedIntervals = [];
-    // merge the newIntervals
+    // loop through `newIntervals`
+    // compare each interval in `newIntervals` with the last interval in `mergedIntervals`
+    // two comparsions:
+    //      if no onverlap ---> push the current interval `newIntervals[i]` into `mergedIntervals`
+    //      if overlap ---> merge the current interval `newIntervals[i]` with the last interval in `mergedIntervals`
     for(let i = 0; i < newIntervals.length; i++){
         let last = mergedIntervals[mergedIntervals.length - 1];
         let current = newIntervals[i];
@@ -15,7 +19,6 @@ var insert = function(intervals, newInterval) {
             mergedIntervals.push(current);
         }else{
             // merge
-           
             last[1] = Math.max(last[1], current[1])
         }
     }
