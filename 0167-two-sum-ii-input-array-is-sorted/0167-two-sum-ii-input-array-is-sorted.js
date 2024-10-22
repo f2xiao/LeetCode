c0d9a575314b7916bf017f1d46f70f6a0b9c0d55
fangfang <25191617+f2xiao@users.noreply.github.com>
2024-10-22 15:33:46 -0400

    Time: 0 ms (100%), Space: 50.1 MB (52.85%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,19 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    const copyNumbers = [...numbers];
-    const pair =[];
-    numbers.forEach((num1, index1)=>{
-        // edge case, the two numbers are not the same
-        for(let j = copyNumbers.length - 1; j > index1; j--){
-            if(num1 === copyNumbers[j] && num1 + copyNumbers[j]!==target){
-                break;
-            }
-            if(num1 + copyNumbers[j]===target){
-                pair.push(index1+1, j+1);
-            }
+    let l = 0;
+    let r = numbers.length - 1;
+    while(l < r){
+        const sum = numbers[l] + numbers[r];
+        if(sum< target){
+            l ++;
+        }else if(sum > target){
+            r--;
+        }else {
+            return [l+1, r+1]
         }
-    })
+    }
     
-    return pair;
 };
