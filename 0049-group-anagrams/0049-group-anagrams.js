521c567bf4ac65790b8dcde629889a68c17251e7
fangfang <25191617+f2xiao@users.noreply.github.com>
2024-11-13 14:35:52 -0500

    Time: 34 ms (66.38%), Space: 64 MB (30.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -3,7 +3,8 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-    const res = {};
+    // const res = {};
+    const map = new Map()
     for(let s of strs){
         const charCount = new Array(26).fill(0);
         for(let c of s){
@@ -11,14 +12,20 @@ var groupAnagrams = function(strs) {
         }
 
         const key = charCount.join(',');
-        if(!res[key]){
-            res[key] = []
-        }
+    //     if(!res[key]){
+    //         res[key] = []
+    //     }
             
-        res[key].push(s);
+    //     res[key].push(s);
         
-    }
+    // }
 
-    return Object.values(res)
+    // return Object.values(res)
+      if (!map.has(key)) {
+            map.set(key, []);
+        }
+        map.get(key).push(s);
+    }
+    return Array.from(map.values());
     
 };
