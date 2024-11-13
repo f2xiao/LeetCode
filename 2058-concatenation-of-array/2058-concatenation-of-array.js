bff270f177d3f5156ae4f264d9511a92d9b92c87
fangfang <25191617+f2xiao@users.noreply.github.com>
2024-11-13 11:21:11 -0500

    Time: 3 ms (17.97%), Space: 52.9 MB (43.23%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.js
+++ 2058-concatenation-of-array/2058-concatenation-of-array.js
@@ -3,7 +3,15 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    // loop through the array
-    return [...nums, ...nums]
+    
+    // return [...nums, ...nums];
+    const ans = [];
+    for(let j = 0; j < 2; j++){
+        for(let i = 0; i < nums.length; i++ ){
+            ans.push(nums[i]);
+        }
+    }
+
+    return ans;
     
 };
